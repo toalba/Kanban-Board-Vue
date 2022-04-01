86550668d287188b42322e5778f76760dbb9148c
toalba <tb@kleinundpartner.at>
2022-04-01 18:24:28 +0200

    So i testet this with a table, but tables are not the best for this, so lets try containers with lists in it

--- src/components/KBoard.vue
+++ src/components/KBoard.vue
@@ -1,11 +1,30 @@
 <template>
-  <div class="container">
-    <table class="table-bordered">
-      <tr>
-        <th scope="col" v-for="erows in krows" :key="erows">{{erows}} </th>
-      </tr>
-    </table>
+  <div>
+
   </div>
+
+
+  <!--<div class="container">
+    <table class="table-bordered">
+      <thead>
+        <tr>
+          <th scope="col" v-for="erows in krows" :key="erows">{{erows}} </th>
+        </tr>
+      </thead>
+      <tbody class="table-striped">
+        <tr v-for="etask in tasks" :key="etask.krow">
+          <th v-if="etask.krow=='Backlog'">{{etask.name}}</th>
+          <th v-else> </th>
+          <th v-if="etask.krow=='Offen'">{{etask.name}}</th>
+          <th v-else> </th>
+          <th v-if="etask.krow=='In Bearbeitung'">{{etask.name}}</th>
+          <th v-else> </th>
+          <th v-if="etask.krow=='Done'">{{etask.name}}</th>
+          <th v-else> </th>
+        </tr>
+      </tbody>
+    </table>
+  </div> --->
 </template>
 
 <script>
@@ -13,7 +32,6 @@ export default {
   name: "KBoard",
   data() {
     return {
-      message: "Hi",
       krows: ["Backlog","Offen","In Bearbeitung","Done"],
       tasks: [
         {name:"Vue verstehen",krow:"In Bearbeitung"},
