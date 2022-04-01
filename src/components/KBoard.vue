5fa970ce23cc8b2c03e58834c5085bb310be55c7
toalba <tb@kleinundpartner.at>
2022-04-01 20:50:57 +0200

    the drag is working but i cant place them

--- src/components/KBoard.vue
+++ src/components/KBoard.vue
@@ -1,47 +1,59 @@
 <template>
-  <div>
-
+  <div class="container">
+    <div class="mb-3">
+      <input class="form-control" v-model="newtask" placeholder="Enter Taks" @keydown.enter="add" >
+    </div>
   </div>
-
-
-  <!--<div class="container">
-    <table class="table-bordered">
-      <thead>
-        <tr>
-          <th scope="col" v-for="erows in krows" :key="erows">{{erows}} </th>
-        </tr>
-      </thead>
-      <tbody class="table-striped">
-        <tr v-for="etask in tasks" :key="etask.krow">
-          <th v-if="etask.krow=='Backlog'">{{etask.name}}</th>
-          <th v-else> </th>
-          <th v-if="etask.krow=='Offen'">{{etask.name}}</th>
-          <th v-else> </th>
-          <th v-if="etask.krow=='In Bearbeitung'">{{etask.name}}</th>
-          <th v-else> </th>
-          <th v-if="etask.krow=='Done'">{{etask.name}}</th>
-          <th v-else> </th>
-        </tr>
-      </tbody>
-    </table>
-  </div> --->
+  <div class="row mt-5">
+    <div v-for="elem in krows" :key="elem" class="col-3">
+      <div class="p-2 alert alert-secondary">
+        <h3>{{ elem }}</h3>
+        <vuedraggable class="list-group kanban-colum" :model-value="tasks" group="aufgaben" @start="drag=true" @end="drag=false" item-key="id">
+          <template  #item="{element}">
+            <div class="list-group-item" v-if="element.krow===elem">{{element.name}}</div>
+          </template>
+        </vuedraggable>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+//import KList from "@/components/KList";
+import vuedraggable from "vuedraggable";
 export default {
+  components: {
+    vuedraggable,
+  },
   name: "KBoard",
   data() {
     return {
+      drag: false,
+      newtask: "",
       krows: ["Backlog","Offen","In Bearbeitung","Done"],
       tasks: [
         {name:"Vue verstehen",krow:"In Bearbeitung"},
         {name:"Kanban bauen",krow:"Offen"}
       ]
     }
+  },
+  methods:{
+    add(){
+      if(this.newtask)
+      {
+        this.tasks.push({name:this.newtask,krow:"Offen"})
+        this.newtask="";
+
+      }
+    }
   }
+
 }
 </script>
 
 <style scoped>
+.kanban-colum{
+  min-height: 300px;
+}
 
 </style>
---
+++ src/components/KList.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="p-2 alert alert-secondary">
+    <h3>{{ elem }}</h3>
+    <vuedraggable class="list-group kanban-colum" :model-value="aufg" group="aufgaben" @start="drag=true" @end="drag=false" item-key="id">
+      <template  #item="{element}">
+        <div v-if="element.krow===elem">{{element.name}}</div>
+      </template>
+    </vuedraggable>
+  </div>
+</template>
+
+<script>
+import vuedraggable from "vuedraggable"
+
+export default {
+  components:{
+    vuedraggable
+  },
+  name: "KList",
+  props: {
+    elem:String,
+    aufg:Object
+  },
+  data() {
+    return {
+      drag: false,
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .kanban-colum{
+    min-height: 300px;
+  }
+</style>
